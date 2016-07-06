4e60bcb0b336bd5ff7158039896fb426a00be715
Christof77 <alexander.val@gmx.net>
2016-07-06 10:36:13 +0200

    5th commit
    from atom

--- Hallo World/src/HelloWorld.java
+++ Hallo World/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World, now on Git. We are on!");
-		System.out.println("And bye, bye!");
+		System.out.println("And bye, bye!!!");
 
 	}
 
