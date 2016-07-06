0acacd3913d8a9e7de94d6a81be01a12c7e9d458
Christof77 <alexander.val@gmx.net>
2016-07-06 09:57:08 +0200

    More text

--- Hallo World/src/HelloWorld.java
+++ Hallo World/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World, now on Git");
+		System.out.println("Hello World, now on Git. We are on!");
 
 	}
 
